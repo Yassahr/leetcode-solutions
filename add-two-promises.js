d71e847f4280ad43236cf5d49a2a3a43b4c7b005
Yassahr <yassahr@gmail.com>
2024-12-15 12:47:54 -0500

    add-two-promises.js

---
+++ add-two-promises.js
@@ -0,0 +1,8 @@
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+
+var addTwoPromises = async function(promise1, promise2) {
+    console.log(promise1) 
+    const prom1= await promise1
+    const prom2= await promise2
+    return prom1 + prom2
+};
