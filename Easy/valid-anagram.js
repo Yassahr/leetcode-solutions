4598231e4bf178aae1c65cf3c881464bdc2c8141
Yassahr <yassahr@gmail.com>
2025-01-17 20:26:55 -0500

    second solution to valid-anagram.js using hash table

--- Easy/valid-anagram.js
+++ Easy/valid-anagram.js
@@ -5,3 +5,28 @@
 function isAnagram(s, t) {
     return s.split('').sort().join('') == t.split('').sort().join('')
 }
+isAnagram(s, t) {
+    //count all of the char in 1 string
+    //compare the counts for char in each string 
+    //return a boolean
+
+    //instatiate an array/hashmap
+    //push all of the values of one of the character to this array
+    //subtract all of the char for other string
+    //->result is each value is equal to zero
+    if(s.length != t.length){ 
+        return false;
+    }
+
+    s= s.toLowerCase();
+    t= t.toLowerCase();
+    let count = new Array(26).fill(0);
+    for(let i=0; i<s.length; i++){
+        //the index of that char in count ++
+        count[s.charCodeAt(i)-'a'.charCodeAt(0)]++
+        count[t.charCodeAt(i)-'a'.charCodeAt(0)]--
+
+    }
+    return count.every(n=> n===0)
+    //every method to check 
+}
