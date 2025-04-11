88bf0d9d66bf42767753dceb2e4e912081e08631
Yassahr <yassahr@gmail.com>
2025-04-10 23:32:50 -0400

    two pointer solution O(1) implemented

---
+++ Meduim/two-sum-part-II.js
@@ -0,0 +1,49 @@
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+ //why does it change that the array is sorted?
+ //this only works if it is sorted
+ var twoSum = function(nums, target) {
+    //establish value for left and right most value
+
+    //while loop to make sure left and right dont cross
+        //check it l+r=target
+        //if more move right
+        //if less move left
+    let l= 0
+    let r= nums.length-1
+
+    while(l<r){
+        let total = nums[l]+ nums[r]
+        console.log(total, l, r)
+        if(total===target){
+            return [l+1,r+1]
+        }else if(total>target){
+            r-=1
+        }else if(total<target){
+            l+=1
+        }
+    }
+    
+
+
+
+    
+};
